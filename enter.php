5277b96631276f169c12ca1880e28839f8fcfea1
aleksOtryshko <55054459+aleksOtryshko@users.noreply.github.com>
2024-10-14 17:09:27 +0300

    Create enter.php

---
+++ enter.php
@@ -0,0 +1,6 @@
+<!-- Форма авторизации отправляет данные на enter_handler.php -->
+<form method="post" action="enter_handler.php">
+    Логин: <input type="text" name="login" required><br>
+    Пароль: <input type="password" name="password" required><br>
+    <input type="submit" value="Войти">
+</form>
