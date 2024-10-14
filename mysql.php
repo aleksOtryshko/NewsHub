df828cda9b858e01b5753b5543d77d9d3f3f2ec6
aleksOtryshko <55054459+aleksOtryshko@users.noreply.github.com>
2024-10-14 16:58:41 +0300

    Create mysql.php

---
+++ mysql.php
@@ -0,0 +1,14 @@
+<?php
+// Подключение к базе данных
+$host = 'localhost';
+$db = 'news_db';
+$user = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Ошибка подключения к базе данных: " . $e->getMessage());
+}
+?>
