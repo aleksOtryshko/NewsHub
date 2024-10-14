3d1d19e0907caa066d86665992e528a381e4c93b
aleksOtryshko <55054459+aleksOtryshko@users.noreply.github.com>
2024-10-14 17:07:31 +0300

    Create reg.php

---
+++ reg.php
@@ -0,0 +1,6 @@
+<!-- Форма регистрации отправляет данные на reg_handler.php -->
+<form method="post" action="reg_handler.php">
+    Логин: <input type="text" name="login" required><br>
+    Пароль: <input type="password" name="password" required><br>
+    <input type="submit" value="Зарегистрироваться">
+</form>
